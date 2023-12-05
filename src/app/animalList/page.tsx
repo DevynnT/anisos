352938fdfca23ad7a7668e5261626210157ddbf3
selinasein <neulmisscj@gmail.com>
2023-12-04 17:48:54 -0800

    landing page, navbar, animal list page done

--- src/app/animalList/page.tsx
+++ src/app/animalList/page.tsx
@@ -1,10 +1,25 @@
 import NavBar from "@/components/NavBar";
+import SearchBar from "@/components/SearchBar";
+import TopBanner from "@/components/TopBanner";
 import Link from "next/link";
+
+import fakeDatabaseAnimal from "@/fakeDbSelina";
+import AnimalListContainer from "@/components/AnimalListContainer";
 
 export default function AnimalList() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
-      <div className="bg-[#DFE3DB] rounded-lg shadow-md p-8 w-full h-screen max-w-md border border-gray-900 overflow-auto relative">
+      <div className="bg-[#DFE3DB] rounded-lg shadow-md p-8 w-full h-screen max-w-md border border-gray-900 relative overflow-hidden">
+        <TopBanner />
+        <div className="m-5">
+          <SearchBar />
+        </div>
+        <div className="overflow-scroll grid grid-cols-2 m-5 gap-5 h-4/5 pb-10 overflow-x-hidden">
+          {fakeDatabaseAnimal.map((animal) => (
+            <AnimalListContainer animal={animal} />
+          ))}
+        </div>
+
         <NavBar />
       </div>
     </div>
