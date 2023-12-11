ed90859172aac27af75a54b92364a0fb4daf24c9
Brian Xu <mattx0121@gmail.com>
2023-12-10 16:17:04 -0800

    fix merge error

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+//
 
 const useCurrentDate = () => {
   const [currentDate, setCurrentDate] = useState('');
